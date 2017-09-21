63da243ba459dd5dc7a3c4936af5ca3bbeb86075
SUKAI at home <huangsukai1997@gmail.com>
2017-09-21 11:28:45 +1000

    test 7 without warning

--- Sources/Order.hs
+++ Sources/Order.hs
@@ -1,7 +1,7 @@
 module Order where
 
 import Types
-import Market
+
 
 -- Change this implementation to your own non-trivial trading strategy.
 -- Do not modify the type signature of the function.
@@ -9,77 +9,87 @@ import Market
 
 -- algorithm of my makeOrders function:
 -- step 1: check what to sell
--- step 2: check what to short-sell
+-- step 2: check what to short-sell and short-sell buy back
 -- step 3: check what to buy
 -- rules:
--- 1. keep 50% of the wealth as cash
--- 2. only 10% of wealth coming from short-sell
+-- 1. try to keep cash more than 0
+-- 2. When the market condition is bad, try to sell more stocks
+
 makeOrders :: Portfolio -> [StockHistory] -> [Order]
 makeOrders po@(cash, _) history
+-- use 4 days to have a trend of the stocks
     | (length $ snd $ head history) < 5 = []
+-- try to keep cash more than 0, thus only execute makeSellOrder and makeShortSellBuyBack
     | cash < 0 = makeSellOrder po history ++ makeShortSellBuyBack po history
-    | marketCondition history == 1 =  makeSellOrderAtBadTime po history ++ makeShortSellOrder po history ++ makeShortSellBuyBack po history ++ makeBuyOrderAtBadTime po history
-    | otherwise = makeSellOrder po history ++ makeShortSellOrder po history ++ makeShortSellBuyBack po history ++ makeBuyOrder po history
+-- when the market condition is bad, the conditions imposed on buying in were stringent, while the conditions on sell out were loosen
+    | marketCondition history == 1 =  makeSellOrderAtBadTime po history ++ makeShortSellOrder 2 po history ++ makeShortSellBuyBack po history ++ makeBuyOrderAtBadTime po history
+    | otherwise = makeSellOrder po history ++ makeShortSellOrder 4 po history ++ makeShortSellBuyBack po history ++ makeBuyOrder po history
 
      where
          makeSellOrder :: Portfolio -> [StockHistory] -> [Order]
-         makeSellOrder po history = case po of
+         makeSellOrder p h = case p of
              (_, []) -> []
              (_, x:xs)
-                 | (getStockPricex 0 x history - getStockPricex 4 x history) / (getStockPricex 0 x history) < (-0.16)  && snd x > 0 -> [Order (fst x) (-snd x)] ++ makeSellOrder (fst po, xs) history
-                 | otherwise -> makeSellOrder (fst po, xs) history
+-- Sell out the stocks when it drop more than 0.16 in ratio within 4 days
+                 | (getStockPricex 0 x h - getStockPricex 4 x h) / (getStockPricex 0 x h) < (-0.16)  && snd x > 0 -> [Order (fst x) (-snd x)] ++ makeSellOrder (fst p, xs) h
+                 | otherwise -> makeSellOrder (fst p, xs) h
 
              where
                    getStockPricex :: Int -> Holding -> [StockHistory] -> Price
-                   getStockPricex index s histories = (snd getStock) !! index
+                   getStockPricex index s hs = (snd getStock) !! index
                        where
-                           getStock = head $ filter (\x -> fst x == fst s) histories
+                           getStock = head $ filter (\x -> fst x == fst s) hs
 
+-- At bad time, sell out the stocks when it drop more than 0.1 in ratio within 4 days
          makeSellOrderAtBadTime:: Portfolio -> [StockHistory] -> [Order]
-         makeSellOrderAtBadTime po history = case po of
+         makeSellOrderAtBadTime p h = case p of
              (_, []) -> []
              (_, x:xs)
-                  | (getStockPricex 0 x history - getStockPricex 4 x history) / (getStockPricex 0 x history)  < (- 0.1)  && snd x > 0 -> [Order (fst x) (-snd x)] ++ makeSellOrderAtBadTime (fst po, xs) history
-                  | otherwise -> makeSellOrderAtBadTime (fst po, xs) history
+                  | (getStockPricex 0 x h - getStockPricex 4 x h) / (getStockPricex 0 x h)  < (- 0.1)  && snd x > 0 -> [Order (fst x) (-snd x)] ++ makeSellOrderAtBadTime (fst p, xs) h
+                  | otherwise -> makeSellOrderAtBadTime (fst p, xs) h
 
              where
                    getStockPricex :: Int -> Holding -> [StockHistory] -> Price
-                   getStockPricex index s histories = (snd getStock) !! index
+                   getStockPricex index s hs = (snd getStock) !! index
                        where
-                           getStock = head $ filter (\x -> fst x == fst s) histories
+                           getStock = head $ filter (\x -> fst x == fst s) hs
 
-         makeShortSellOrder :: Portfolio -> [StockHistory] -> [Order]
-         makeShortSellOrder po@(cash, _) history = case history of
+-- Short sell the stocks when it continues dropping within x days
+         makeShortSellOrder :: Int -> Portfolio -> [StockHistory] -> [Order]
+         makeShortSellOrder num port@(cashs, _) h = case h of
              []   -> []
              (s,p):xs
-                 | keyDay p 2 && not (elem s (map fst (snd po)))  -> [Order s (abs(floor (cash * 0.2 / head p)))] ++ makeShortSellOrder po xs
-                 | otherwise -> makeShortSellOrder po xs
+                 | keyDay p num && not (elem s (map fst (snd port)))  -> [Order s (abs(floor (cashs * 0.2 / head p)))] ++ makeShortSellOrder num port xs
+                 | otherwise -> makeShortSellOrder num port xs
              where
                  keyDay :: [Price] -> Int -> Bool
                  keyDay p x
                      | x <= 1 = p !! 1 > p !! 0
                      | otherwise = p !! x > p !!(x-1) && keyDay p (x-1)
 
+-- Short sell buy back when the short-sell stock drop 0.16 in ratio within 4 days
          makeShortSellBuyBack :: Portfolio -> [StockHistory] -> [Order]
-         makeShortSellBuyBack po history = case po of
+         makeShortSellBuyBack p h = case p of
             (_, []) -> []
             (_, x:xs)
-                 | snd x < 0 && ((snd $ head $ filter (\y -> fst y == fst x) history)!!0 - (snd $ head $ filter (\y -> fst y == fst x) history)!! 4)/ (snd $ head $ filter (\y -> fst y == fst x) history)!!0 < (-0.15) -> [Order (fst x) (-snd x) ] ++ makeShortSellBuyBack (fst po, xs) history
-                 | otherwise -> makeShortSellBuyBack (fst po, xs) history
+                 | snd x < 0 && ((snd $ head $ filter (\y -> fst y == fst x) h)!!0 - (snd $ head $ filter (\y -> fst y == fst x) h)!! 4)/ (snd $ head $ filter (\y -> fst y == fst x) h)!!0 < (-0.16) -> [Order (fst x) (-snd x) ] ++ makeShortSellBuyBack (fst p, xs) h
+                 | otherwise -> makeShortSellBuyBack (fst p, xs) h
 
+-- Buy in stocks when it's price increase in 0.11 in ratio within 4 days
          makeBuyOrder :: Portfolio -> [StockHistory] -> [Order]
-         makeBuyOrder po history = case history of
+         makeBuyOrder port@(cashs, _) h = case h of
               []   -> []
               (s,p):xs
-                   | (head p -  p!!4)/ (head p) > 0.11 -> [Order s (abs (floor (cash / 4 / head p)))] ++ makeBuyOrder po xs
-                   | otherwise -> makeBuyOrder po xs
+                   | (head p -  p!!4)/ (head p) > 0.11 -> [Order s (abs (floor (cashs / 4 / head p)))] ++ makeBuyOrder port xs
+                   | otherwise -> makeBuyOrder port xs
 
+-- At bad time, only buy in the stocks when the price continues increasing and increase in 0.11 in ratio within 4 days
          makeBuyOrderAtBadTime :: Portfolio -> [StockHistory] -> [Order]
-         makeBuyOrderAtBadTime po history = case history of
+         makeBuyOrderAtBadTime port@(cashs, _) h = case h of
               []   -> []
               (s,p):xs
-                   | (head p -  p!!4)/ (head p) > 0.11 && (keyDay' p 4) -> [Order s (abs (floor (cash / 4 / head p)))] ++ makeBuyOrderAtBadTime po xs
-                   | otherwise -> makeBuyOrderAtBadTime po xs
+                   | (head p -  p!!4)/ (head p) > 0.11 && (keyDay' p 4) -> [Order s (abs (floor (cashs / 4 / head p)))] ++ makeBuyOrderAtBadTime port xs
+                   | otherwise -> makeBuyOrderAtBadTime port xs
               where
                  keyDay' :: [Price] -> Int -> Bool
                  keyDay' p x
@@ -87,10 +97,10 @@ makeOrders po@(cash, _) history
                      | otherwise = p !! x < p !!(x-1) && keyDay' p (x-1)
 
 
--- 0 for good, 1 for bad
+-- Check market condition, 0 for good, 1 for bad, it is bad when 80 percent of the stocks continue dropping in 2 days.
          marketCondition :: [StockHistory] -> Integer
-         marketCondition history
-             | ((fromIntegral $ length(filter (\x -> x == True) (map whetherContinueDrop (map snd history)))) / (fromIntegral $ length((map whetherContinueDrop (map snd history))))) > 0.8 = 1
+         marketCondition h
+             | ((fromIntegral $ length(filter (\x -> x == True) (map whetherContinueDrop (map snd h)))) / (fromIntegral $ length((map whetherContinueDrop (map snd h))))) > 0.8 = 1
              | otherwise = 0
 
              where
@@ -98,14 +108,6 @@ makeOrders po@(cash, _) history
                  whetherContinueDrop p
                      | p !! 2 > p !! 1 && p !! 1 > p !! 0 = True
                      | otherwise = False
-
---         sellThemAll :: Portfolio -> [Order]
---         sellThemAll po@(cash,holding) = case holding of
---             [] -> []
---             (s, q):xs
---                 | q > 0 -> [Order s (-q)] ++ sellThemAll (cash, xs)
---                 | otherwise -> sellThemAll (cash, xs)
-
 
 
 
